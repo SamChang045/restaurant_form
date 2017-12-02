dcc63538d202754509b59edf3bca6e287d269a54
SamChang <willpower03@gmail.com>
2017-12-02 09:33:10 +0800

    add restaurant index action and view for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -3,7 +3,11 @@ class Admin::RestaurantsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
   
+
+  #由於是全部 (多筆) 餐廳資料，所以實例變數 ＠restaurants 使用複數。
+  #用 Restaurant.all 撈出所有的餐廳資料並存入 @restaurants 這個實例變數。
   def index
+    @restaurants = Restaurant.all
   end
   
 end
