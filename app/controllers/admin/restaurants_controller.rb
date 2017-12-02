c0c01b1e28b8139195b0291b974d1458aed0c3f4
SamChang <willpower03@gmail.com>
2017-12-02 09:43:34 +0800

    update add restaurant create actions for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -15,4 +15,26 @@ class Admin::RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  #你必須將寫入參數的程序，從 new action 裡獨立出來，宣告成另一個私有方法。這裡依慣例命名為 restaurant_params。
+  #使用 params 取出從 Client 端送進的 Request 裡，Controller 所需參數。
+  #使用 require(:object_name) 拿出表單資料。
+  #使用 permit 允許指定的屬性資料傳件 Model。
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "restaurant was failed to create"
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
+
 end
