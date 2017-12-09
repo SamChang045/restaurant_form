d2e23cbb828d0d7880a0e0c31a065d00ae207083
SamChang045 <willpower03@gmail.com>
2017-12-09 14:34:51 +0800

    user can update category with the same form on index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -17,15 +17,40 @@ class Admin::CategoriesController < ApplicationController
   #
   #
   #
+  #修改 index action，傳入 form_for 需要的參數
+  #為解決 form_for 沒有 @category 的問題，請你跟著以下步驟操作：
+  #在 index action 中宣告 @category
+  #透過 new 方法新增一個空的 Category 物件
   def index
     @categories = Category.all
-    @category = Category.new
+    if params[:id]
+      @category = Category.find(params[:id])
+    else
+      @category = Category.new
+    end
   end
 
+  #我們需要使用 Strong parameter 來允許表單傳入資料，因此會另外建立一個 category_params 方法，並設定為私有方法。
+  #儲存失敗時，由於要重新 render index 樣板，需要再額外傳入 index 需要的 @categories 實例變數。
   def create
     @category = Category.new(category_params)
     if @category.save
       flash[:notice] = "category was successfully created"
+      redirect_to admin_categories_path
+    else
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  #實作 update action 和 create action 的邏輯相似——首先，#我們需要找到特定的 Category 資料，
+  #並且寫一個 if...else 條件式，  #如果通過 Model 的驗證，成功儲存到資料庫裡，就導向適合的頁面，
+  #並留給使用者成功訊息；如果不成功，就重新 render index 樣板，並傳入 index 樣板需要的 @categories 變數，
+  #使用者有重新輸入的機會。
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "category was successfully updated"
       redirect_to admin_categories_path
     else
       @categories = Category.all
