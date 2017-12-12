ab5fd85d18f0477775c4614bf6bc899ba594f6a4
SamChang045 <willpower03@gmail.com>
2017-12-12 20:34:57 +0800

    setup restaurant resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #根據 RESTful 設計，resources 會開放 7 種 action，然而，前台使用者的行為只有 index action 和 show action。因此我們使用 :only 來限制要宣告的資源。
+  resources :restaurants, only: [:index, :show]
   root "restaurants#index"
 
   namespace :admin do
