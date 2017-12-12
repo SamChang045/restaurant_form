498254db37e35e653f26658722e610cb0dce5746
SamChang045 <willpower03@gmail.com>
2017-12-12 22:59:37 +0800

    setup category resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   #根據 RESTful 設計，resources 會開放 7 種 action，然而，前台使用者的行為只有 index action 和 show action。因此我們使用 :only 來限制要宣告的資源。
   resources :restaurants, only: [:index, :show]
+  resources :categories, only: :show
   root "restaurants#index"
 
   namespace :admin do
