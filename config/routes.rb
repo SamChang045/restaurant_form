335cbd4fcab06795ee21d63ccc643b04df95f39d
SamChang045 <willpower03@gmail.com>
2017-12-29 09:58:15 +0800

    rails generate controller comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #根據 RESTful 設計，resources 會開放 7 種 action，然而，前台使用者的行為只有 index action 和 show action。因此我們使用 :only 來限制要宣告的資源。
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:index, :show] do
+    resources :comments, only: [:create, :destroy]
+  end
   resources :categories, only: :show
   root "restaurants#index"
 
