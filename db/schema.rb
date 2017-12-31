9aa7a9006fe381c98d901100e24c5b9f0da67de5
SamChang045 <willpower03@gmail.com>
2017-12-31 15:52:34 +0800

    customize the registration form with gender and birth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230150133) do
+ActiveRecord::Schema.define(version: 20171231074011) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20171230150133) do
     t.string "name"
     t.text "intro"
     t.string "avatar"
+    t.datetime "date_of_birth"
+    t.boolean "is_female", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
