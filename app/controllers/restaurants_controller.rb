44801b0b665169f45ce34891d30d44e7ad0950e0
SamChang045 <willpower03@gmail.com>
2018-01-02 22:18:20 +0800

    user can link to dashboard from restaurant show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,4 +14,10 @@ class RestaurantsController < ApplicationController
     @recent_comments = Comment.order(created_at: :desc).limit(10)
   end
 
+  # GET restaurants/dashboard
+   # 會去 render app/views/restuarants/dashboard.html.erb
+  def dashboard
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
