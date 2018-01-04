f6d8939f567fbd56b4cf7a0ff063696e76733323
SamChang045 <willpower03@gmail.com>
2018-01-04 23:30:16 +0800

    add favorite and unfavorite action on RestaurantsController

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,24 @@ Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
 
-    # 瀏覽所有餐廳的最新動態
     collection do
+      # 瀏覽所有餐廳的最新動態
+      # GET restaurants/feeds
       get :feeds
     end
 
-    # 瀏覽個別餐廳的 Dashboard
+
     member do
+
+      # 瀏覽個別餐廳的 Dashboard
+      # GET restaurants/:id/dashboard
       get :dashboard
+
+      # 收藏 / 取消收藏
+      post :favorite
+      post :unfavorite
     end
-    
+
   end
 
   resources :users, only: [:show, :edit, :update]
