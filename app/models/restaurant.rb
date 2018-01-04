8de8b3a4be8b1ac93dc4593cf4e2207b83d1ae72
SamChang045 <willpower03@gmail.com>
2018-01-04 23:15:51 +0800

    normalization the naming rule of the relationship about user and comment

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,7 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name,:tel,:address,:opening_hours,:description
   mount_uploader :image, PhotoUploader
   belongs_to :category
+  
   has_many :comments, dependent: :destroy
 
   has_many :favorites, dependent: :destroy
