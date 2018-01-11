7c550aee8b36bdd9c99509537e88487c9d92182f
SamChang045 <willpower03@gmail.com>
2018-01-11 23:51:40 +0800

    styled user index page

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -23,8 +23,4 @@ class FollowshipsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
-  def following?(user)
-    self.followings.include?(user)
-  end
-
 end
