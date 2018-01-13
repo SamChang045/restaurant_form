6edc6b5615637e000a880ca62183843556a33ba6
SamChang045 <willpower03@gmail.com>
2018-01-13 23:56:49 +0800

    validate uniqueness of friending_id in user_id scope

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,8 @@
 class Friendship < ApplicationRecord
 
+  # 確保特定 user_id 下，只能有一個 friendings_id
+  validates :friending_id, uniqueness: { scope: :user_id }
+
   belongs_to :user
   belongs_to :friending, class_name: "User"
 
