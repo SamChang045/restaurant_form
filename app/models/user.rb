7312009a1943efd87611d85a0358684741faa6a6
SamChang045 <willpower03@gmail.com>
2018-01-14 00:14:57 +0800

    add RelationshipConfirm? method to user model for checking friend and unfriendbutton

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,5 +40,9 @@ class User < ApplicationRecord
   def following?(user)
     self.followings.include?(user)
   end
+  
+  def RelationshipsConfirming?(user)
+    self.friendings.include?(user)
+  end
 
 end
